fc10e9621fffc40cad598ffed0ea7171fe7b3d0a
Gilles Darold <gilles@darold.net>
2026-06-17 22:00:26 +0700

    Add new version of SQL files

---
+++ updates/pg_dbms_errlog--2.2--2.3.sql
@@ -0,0 +1,5 @@
+---------------------------------------------------------------------------------
+-- pg_dbms_errlog extension for PostgreSQL
+--	Emulate DBMS_ERRLOG Oracle module but in a simplistic way.
+---------------------------------------------------------------------------------
+
